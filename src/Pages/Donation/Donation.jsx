d3dbbb93ca449d9848b19d84591fa476ca92df29
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-09-25 17:24:32 +0600

    updated banner content and styles

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,23 +4,31 @@ import DonationCard from "./DonationCard";
 
 const Donation = () => {
     const [donations, setDonations] = useState([])
+    const [donationLength, setDonationLength] = useState(4)
     useEffect(() => {
         const donationItems = JSON.parse(localStorage.getItem('donation'))
         if (donationItems) {
             setDonations(donationItems)
-        } else{
+        } else {
             console.log('no data');
         }
     }, [])
     return (
         <div>
-          
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-            {
-                donations.map(donation => 
-                <DonationCard key={donation.id} donation={donation}></DonationCard>)
-            }
-           </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+                {
+                    donations.slice(0, donationLength).map(donation =>
+                        <DonationCard key={donation.id} donation={donation}></DonationCard>)
+                }
+
+
+            </div>
+
+            <div className={donationLength === donations.length ? 'hidden' : ''}>
+                <button onClick={() => setDonationLength(donations.length)}
+                className="btn btn-primary ">Show all</button>
+            </div>
         </div>
     );
 };
--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,8 +2,8 @@
 
 const Banner = () => {
     return (
-        <div className="h-[70vh] bg-green-300">
-            <div className="hero h-[70vh] bg-white" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+        <div className="h-[70vh]">
+            <div className="hero h-[70vh] bg-opacity-100" style={{backgroundImage: 'url(https://i.ibb.co/2NNRxN2/Rectangle-4281.png)'}}>
                 <div className="hero-content text-center">
                     <div className="">
                         <h1 className="text-5xl text-[#0B0B0B] font-bold py-10">I Grow By Helping People In Need</h1>
